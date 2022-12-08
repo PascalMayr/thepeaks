925813d5845e3026c7a9aab73c23c2a2ca5c6d02
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-08 18:25:56 +0100

    add search

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,14 @@
 import styles from '../../styles/components/header/index.module.css'
 import Logo from '../../assets/logo.svg'
 import Link from 'next/link'
+import Search from '../search'
 
 const Header: React.FC = () => (
   <header className={styles.header}>
     <Link href="/">
       <Logo className={styles.logo} />
     </Link>
+    <Search />
   </header>
 )
 
---
+++ components/search/index.tsx
@@ -0,0 +1,36 @@
+import styles from '../../styles/components/search/index.module.css'
+import SearchIcon from '../../assets/search.svg'
+import { ChangeEvent, useCallback, useState } from 'react'
+import classNames from 'classnames'
+
+const Search: React.FC = () => {
+  const [search, setSearch] = useState<string>('')
+  const [focused, setFocused] = useState<boolean>(false)
+  const handleClick = useCallback(() => setFocused(!focused), [focused])
+  const handleSearch = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value),
+    []
+  )
+  return (
+    <div
+      onClick={handleClick}
+      className={classNames(styles.search, focused && styles.focused)}
+    >
+      <div className={styles.container}>
+        {focused && (
+          <input
+            autoFocus
+            placeholder="Search all news"
+            className={styles.input}
+            onChange={handleSearch}
+            type="text"
+            value={search}
+          />
+        )}
+        <SearchIcon className={styles.icon} />
+      </div>
+    </div>
+  )
+}
+
+export default Search
