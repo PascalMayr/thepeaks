b6382c9e5024819d0686a068639040151fa70802
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-09 23:04:16 +0100

    add ListHeader component

---
+++ components/listHeader/index.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from '../../styles/components/listHeader/index.module.css'
+import BookmarkButton from '../bookmarkButton'
+import Select from '../select'
+
+interface ListHeaderProps {
+  title: string
+}
+
+const ListHeader: React.FC<ListHeaderProps> = ({ title }) => {
+  const router = useRouter()
+  const sortOptions = ['Newest first', 'Oldest first']
+  const selectHandler = (selectedIndex: number) => {
+    const oldOrderBy = router.query['order-by']
+    const newOrderBy = sortOptions[selectedIndex].split(' ')[0].toLowerCase()
+    /* update the page only if the sort option changes, avoid rendering updates */
+    if (
+      (oldOrderBy && newOrderBy !== oldOrderBy) ||
+      (!oldOrderBy && selectedIndex > 0)
+    ) {
+      router.push({ query: { ...router.query, ['order-by']: newOrderBy } })
+    }
+  }
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles['bookmark-container']}>
+        <Link href="/bookmarks">
+          <BookmarkButton title="View Bookmark" />
+        </Link>
+        <Select options={sortOptions} onSelect={selectHandler} />
+      </div>
+    </div>
+  )
+}
+
+export default ListHeader
