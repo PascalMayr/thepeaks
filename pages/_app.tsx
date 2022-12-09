1036b807489aaecc87e6c7f15c2273237a2a2da2
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-09 22:58:37 +0100

    change function component syntax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,50 +5,50 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 import styles from '../styles/pages/app.module.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>The Peaks</title>
-        <meta name="description" content="Latest News" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com/"
-          crossOrigin="true"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.cdnfonts.com/"
-          crossOrigin="true"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
-      </Head>
-      <Header />
-      <div className={styles.page}>
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-    </>
-  )
-}
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>The Peaks</title>
+      <meta name="description" content="Latest News" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com/"
+        crossOrigin="true"
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.cdnfonts.com/"
+        crossOrigin="true"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
+    </Head>
+    <Header />
+    <div className={styles.page}>
+      <Component {...pageProps} />
+    </div>
+    <Footer />
+  </>
+)
+
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <></>
+import ListHeader from '../components/listHeader'
+import { Result } from '../types'
+
+export async function getServerSideProps() {
+  return {
+    props: {},
+  }
 }
+
+interface HomeProps {
+  results: {
+    top: Result[]
+    sport: Result[]
+  }
+}
+
+const Home: React.FC<HomeProps> = () => (
+  <>
+    <ListHeader title="Top Stories" />
+  </>
+)
+
+export default Home
