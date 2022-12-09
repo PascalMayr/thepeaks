fbaec1486577d060517bf2173ca1efa5d5b89816
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-09 23:05:18 +0100

    add api response types

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+enum Pillar {
+  Arts,
+  Sport,
+  News,
+  Opinion,
+}
+
+export interface Result {
+  id: string
+  type: string
+  sectionId: string
+  sectionName: string
+  webPublicationDate: string
+  webTitle: string
+  webUrl: string
+  apiUrl: string
+  isHosted: boolean
+  pillarId: string
+  pillarName: Pillar
+}
+
+export interface ContentResponse {
+  response: {
+    status: string
+    userTier: string
+    total: number
+    startIndex: number
+    pageSize: number
+    currentPage: number
+    pages: number
+    orderBy: string
+    results: Result[]
+  }
+}
