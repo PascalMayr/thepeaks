3d1fa6041a28797903216dbfe8d4bc573bd73fd8
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-10 01:41:13 +0100

    add search page

---
+++ pages/search.tsx
@@ -0,0 +1,11 @@
+import ListHeader from '../components/listHeader'
+
+const Search: React.FC = () => {
+  return (
+    <>
+      <ListHeader title="Search results" />
+    </>
+  )
+}
+
+export default Search
