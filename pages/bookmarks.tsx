208e94851294672bbb0824851dbed286c2bce635
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-10 01:41:26 +0100

    add bookmark page

---
+++ pages/bookmarks.tsx
@@ -0,0 +1,11 @@
+import ListHeader from '../components/listHeader'
+
+const Bookmarks: React.FC = () => {
+  return (
+    <>
+      <ListHeader title="All bookmarks" />
+    </>
+  )
+}
+
+export default Bookmarks
