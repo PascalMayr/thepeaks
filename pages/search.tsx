bf794c1341ddd49e66fd06154169d0e00561d559
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-12 02:02:02 +0100

    add search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,65 @@
+import Head from 'next/head'
+import { ParsedUrlQuery } from 'querystring'
+import Article, { ArticleProps, ArticleVariation } from '../components/article'
 import ListHeader from '../components/listHeader'
+import { SearchResponse, SearchResult } from '../types'
+import api from '../utils/api'
+import styles from '../styles/pages/search.module.css'
 
-const Search: React.FC = () => {
+interface Query extends ParsedUrlQuery {
+  q: string
+}
+
+export async function getServerSideProps({ query: { q } }: { query: Query }) {
+  const remapFields = ({
+    webTitle,
+    id,
+    sectionId,
+    fields,
+    pillarName,
+  }: SearchResult) => ({
+    webTitle,
+    id,
+    sectionId,
+    fields: fields || {},
+    pillarName,
+  })
+  const results = (
+    await api<SearchResponse>('search', {
+      q,
+      ['show-fields']: 'thumbnail',
+      ['page-size']: 15,
+    })
+  ).response.results.map(remapFields)
+  return {
+    props: {
+      q,
+      results,
+    },
+  }
+}
+
+interface SearchProps {
+  q: string
+  results: ArticleProps[]
+}
+
+const Search: React.FC<SearchProps> = ({ q, results }) => {
   return (
     <>
+      <Head>
+        <title>The Peaks - Search {q}</title>
+      </Head>
       <ListHeader title="Search results" />
+      <div className={styles.results}>
+        {results.map((result) => (
+          <Article
+            key={result.id}
+            {...result}
+            variation={ArticleVariation.SMALL}
+          />
+        ))}
+      </div>
     </>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -17,6 +17,9 @@ export interface Result {
   isHosted: boolean
   pillarId: string
   pillarName: Pillar
+}
+
+export interface ContentResult extends Result {
   fields: {
     thumbnail?: string
     trailText: string
@@ -33,22 +36,31 @@ export interface ContentResponse {
     currentPage: number
     pages: number
     orderBy: string
-    results: Result[]
+    results: ContentResult[]
   }
 }
 
-export interface ArticleContent {
-  id: string
-  type: string
-  sectionId: string
-  sectionName: string
-  webPublicationDate: string
-  webTitle: string
-  webUrl: string
-  apiUrl: string
-  isHosted: boolean
-  pillarId: string
-  pillarName: string
+export interface SearchResult extends Result {
+  fields: {
+    thumbnail?: string
+  }
+}
+
+export interface SearchResponse {
+  response: {
+    status: string
+    userTier: string
+    total: number
+    startIndex: number
+    pageSize: number
+    currentPage: number
+    pages: number
+    orderBy: string
+    results: SearchResult[]
+  }
+}
+
+export interface ArticleContent extends Result {
   fields: {
     thumbnail?: string
     trailText: string
