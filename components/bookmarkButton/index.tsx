a72143e7e1c9d25dceeeee6e398b3f561066c6a5
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-12 23:34:38 +0100

    add let users bookmark articles

--- components/bookmarkButton/index.tsx
+++ components/bookmarkButton/index.tsx
@@ -1,17 +1,41 @@
 import styles from '../../styles/components/bookmarkButton/index.module.css'
 import BookMarkIcon from '../../assets/bookmark.svg'
-import { MouseEventHandler } from 'react'
+import { CSSProperties, MouseEventHandler } from 'react'
+import classNames from 'classnames'
 
 interface BookmarkButtonProps {
   title: string
   onClick?: MouseEventHandler<HTMLDivElement>
+  className?: string
+  disabled?: boolean
+  style?: CSSProperties
 }
 
-const BookmarkButton: React.FC<BookmarkButtonProps> = ({ title, onClick }) => {
+const BookmarkButton: React.FC<BookmarkButtonProps> = ({
+  title,
+  onClick,
+  className,
+  disabled,
+  style = {},
+}) => {
   return (
-    <div onClick={onClick} className={styles.bookmark}>
-      <BookMarkIcon /> {title}
-    </div>
+    <>
+      {disabled ? (
+        <div
+          style={style}
+          className={classNames(styles.bookmark, className, styles.disabled)}
+        >
+          <BookMarkIcon /> {title}
+        </div>
+      ) : (
+        <div
+          onClick={onClick}
+          className={classNames(styles.bookmark, className)}
+        >
+          <BookMarkIcon /> {title}
+        </div>
+      )}
+    </>
   )
 }
 
--- pages/article/index.tsx
+++ pages/article/index.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import Head from 'next/head'
 import { ParsedUrlQuery } from 'querystring'
 import { useCallback, useEffect, useState } from 'react'
@@ -16,14 +17,14 @@ export async function getServerSideProps({ query }: { query: Query }) {
     await api<ArticleContentResponse>(query.id, {
       ['show-fields']: 'thumbnail,trailText,body',
     })
-  ).response.content
+  )?.response.content
   return {
     props: content,
   }
 }
 
 const ArticlePage: React.FC<ArticleContent> = (props) => {
-  const { webPublicationDate, webTitle, fields } = props
+  const { webPublicationDate, webTitle, fields, id } = props
   const [body, setBody] = useState<string>(fields?.body || '')
   const [bodyImages, setBodyImages] = useState<string>('')
 
@@ -46,7 +47,7 @@ const ArticlePage: React.FC<ArticleContent> = (props) => {
       setBodyWithoutImages(fields?.body)
     }
   }, [fields?.body, setBodyWithoutImages])
-  /* put images back into the body on mobile */
+  /* put images back into the body on mobile when resizing */
   useEffect(() => {
     const resizeCallback = () => {
       if (window.innerWidth < 818) {
@@ -80,35 +81,100 @@ const ArticlePage: React.FC<ArticleContent> = (props) => {
     )} ${date[5]}`
   }, [])
 
+  const [added, setAdded] = useState(false)
+  const [showHint, setShowHint] = useState(false)
+
+  const addBookmark = useCallback(async () => {
+    localStorage.setItem('bookmarks', JSON.stringify([id]))
+    setAdded(true)
+    setShowHint(true)
+    const hinttimeout = setTimeout(() => {
+      clearTimeout(hinttimeout)
+      setShowHint(false)
+    }, 3000)
+  }, [id])
+
+  const getBookmarks = useCallback(
+    (): string[] => JSON.parse(localStorage.getItem('bookmarks') || '[]'),
+    []
+  )
+
+  const removeBookmark = useCallback(async () => {
+    const bookmarks = getBookmarks()
+    localStorage.setItem(
+      'bookmarks',
+      JSON.stringify(bookmarks.filter((bookmark) => bookmark !== id))
+    )
+    setAdded(false)
+    setShowHint(true)
+    const hinttimeout = setTimeout(() => {
+      clearTimeout(hinttimeout)
+      setShowHint(false)
+    }, 3000)
+  }, [id, getBookmarks])
+
+  const handleBookmarkClick = useCallback(() => {
+    if (!added) addBookmark()
+    else removeBookmark()
+  }, [added, addBookmark, removeBookmark])
+
+  useEffect(() => {
+    const bookmarks = getBookmarks()
+    if (!added && bookmarks.includes(id)) {
+      setAdded(true)
+    }
+  }, [id, added, getBookmarks])
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>{webTitle}</title>
-      </Head>
-      <BookmarkButton title="Add Bookmark" />
-      <p className={styles.date}>{formatDate(webPublicationDate)}</p>
-      <div>
-        <div className={styles.headline}>
-          <h1 className={styles.title}>{webTitle}</h1>
-          {/* headline is the same as webTitle usnig trailText instead.
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>{webTitle}</title>
+        </Head>
+        <BookmarkButton
+          onClick={handleBookmarkClick}
+          title={added ? 'Remove Bookmark' : 'Add Bookmark'}
+          className={classNames(added && styles['remove-bookmark'])}
+        />
+        <p className={styles.date}>{formatDate(webPublicationDate)}</p>
+        <div>
+          <div className={styles.headline}>
+            <h1 className={styles.title}>{webTitle}</h1>
+            {/* headline is the same as webTitle usnig trailText instead.
           trailtext can be html returned from the API */}
-          <p
-            className={styles['trail-text']}
-            dangerouslySetInnerHTML={{ __html: fields?.trailText }}
-          />
-        </div>
-        <div className={styles['content-image-container']}>
-          <div
-            className={styles.content}
-            dangerouslySetInnerHTML={{ __html: body }}
-          />
-          <div
-            className={styles['image-container']}
-            dangerouslySetInnerHTML={{ __html: bodyImages }}
-          />
+            <p
+              className={styles['trail-text']}
+              dangerouslySetInnerHTML={{ __html: fields?.trailText }}
+            />
+          </div>
+          <div className={styles['content-image-container']}>
+            <div
+              className={styles.content}
+              dangerouslySetInnerHTML={{ __html: body }}
+            />
+            <div
+              className={styles['image-container']}
+              dangerouslySetInnerHTML={{ __html: bodyImages }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      {showHint && (
+        <BookmarkButton
+          style={
+            typeof window !== 'undefined'
+              ? { top: Number(window?.innerHeight) - 31.875 }
+              : {}
+          }
+          className={classNames(
+            styles['bookmark-hint'],
+            !added && styles['bookmark-hint-removed']
+          )}
+          title={added ? 'Bookmark added' : 'Bookmark removed'}
+          disabled
+        />
+      )}
+    </>
   )
 }
 
