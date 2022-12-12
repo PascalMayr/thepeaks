886fcdc302c18015e5cf3adb2d1552a8caff51a1
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-12 19:30:31 +0100

    change replace loading svg with div

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,9 +1,8 @@
-import LoadingIcon from '../../assets/loading.svg'
 import styles from '../../styles/components/loading/index.module.css'
 
 const Loading: React.FC = () => (
   <div className={styles.container}>
-    <LoadingIcon className={styles.circle} />
+    <div className={styles.loader} />
   </div>
 )
 
