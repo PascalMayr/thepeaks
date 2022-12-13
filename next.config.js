b5592a09baf1edc02ede41063d5956337806b8de
Pascal Mayr <pascalmayr2@gmail.com>
2022-12-13 13:13:42 +0100

    remove defaultRuntimeCaching

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultRuntimeCaching = require('next-pwa/cache')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
